85850cecc709b38b56ef56c6103a3c2f42586a1e
Fernando Soares <fernando.git898@gmail.com>
2022-11-27 04:14:22 -0300

    Foi adicionado a função de filtrar os itens recebidos no array

--- pages/journey/index.js
+++ pages/journey/index.js
@@ -1,6 +1,37 @@
+import { useState } from 'react';
 import Menu from '../../components/Menu';
 
 export default function Journey() {
+
+    const [JourneySearch, setJourneySearch] = useState('');
+
+    function onSubmit(event) {
+        event.preventDefault();
+    }
+    
+    let journeyList = [
+        {
+            id: 1,
+            name: 'Cobrança Cartão de Crédito E-mail',
+            status: 'Ativo'
+        },
+        {
+            id: 2,
+            name: 'Cobrança Cartão de Crédito SMS',
+            status: 'Testando'
+        },
+        {
+            id: 3,
+            name: 'Boas Vindas E-mail',
+            status: 'Configurando'
+        },
+        {
+            id: 4,
+            name: 'Boas Vindas SMS',
+            status: 'Ativo'
+        }
+    ].filter((e) => e.name.includes(JourneySearch))
+    
     return (
         <>
             <Menu />
@@ -14,35 +45,43 @@ export default function Journey() {
                 </div>
             </div>
 
-            <table className='table container px-4 py-5'>
-                <thead>
-                    <tr>
-                        <th scope='col'>#</th>
-                        <th scope='col'>First</th>
-                        <th scope='col'>Last</th>
-                        <th scope='col'>Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope='row'>1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope='row'>2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope='row'>3</th>
-                        <td colSpan='2'>Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className='container px-4 py-1'>
+
+                <form onSubmit={onSubmit}>
+                    <div className='form-floating mb-3'>
+                        <input type='text' className='form-control' id='floatingInput' onChange={(event) => setJourneySearch(event.target.value)}/>
+                        <label htmlFor='floatingInput'>Journey Search</label>
+                    </div>
+                </form>
+
+            </div>
+
+            <div className='container px-4 py-2'>
+
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th scope='col'>ID</th>
+                            <th scope='col'>Name</th>
+                            <th scope='col'>Status</th>
+                            <th scope='col'>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {journeyList.map((data) => (
+                            <tr key={data.id}>
+                            <th scope='row'>{data.id}</th>
+                            <td>{data.name}</td>
+                            <td>{data.status}</td>
+                            <td>
+                                <button type="button" className="btn btn-primary">Settings</button>
+                            </td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+            </div>
         </>
     )
 }
