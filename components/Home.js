bab1e4498ba1afde698d555e0da546fbabba70cc
Fernando Soares <fernando.git898@gmail.com>
2022-12-01 23:41:08 -0300

    Foi criado a tela de Login

--- components/Home.js
+++ components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
             <div className='row row-cols-1 row-cols-md-2 align-items-md-center g-5 py-5'>
                 <div className='d-flex flex-column align-items-start gap-2'>
                     <h3 className='fw-bold'>Left-aligned title explaining these awesome features</h3>
-                    <p className='text-muted'>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+                    <p className='text-muted'>Paragraph of text beneath the heading to explain the heading. Well add onto it with another sentence and probably just keep going until we run out of words.</p>
                     <a href='#' className='btn btn-primary btn-lg'>Support Contact</a>
                 </div>
                 <div className='row row-cols-1 row-cols-sm-2 g-4'>
