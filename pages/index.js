730f9f586eb7f31036010482a4fe2c8103014d55
Fernando Soares <fernando.git898@gmail.com>
2022-12-01 23:49:42 -0300

    Foi criado a tela de Login

--- pages/index.js
+++ pages/index.js
@@ -33,11 +33,11 @@ export default function App() {
               <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
               <div className="form-floating py-1">
-                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" name='user' onChange={onChange} />
+                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" name='user' autoComplete={false} onChange={onChange} />
                   <label htmlFor="floatingInput">Email address</label>
               </div>
               <div className="form-floating py-1">
-                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" name='password' onChange={onChange} />
+                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" name='password' autoComplete={false} onChange={onChange} />
                   <label htmlFor="floatingPassword">Password</label>
               </div>
 
